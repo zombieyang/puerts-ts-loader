eed2255dd88a489a3625c7cc5c94ebf26ae458a3
zombieyang(杨浩) <zombieyang@tencent.com>
2023-02-20 13:31:09 +0800

    add UseRuntimeLoader and sample

--- upm/Runtime/TSLoader.cs
+++ upm/Runtime/TSLoader.cs
@@ -1,27 +1,33 @@
 using UnityEngine;
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Puerts
 {
     public class TSLoader : Puerts.ILoader, Puerts.IModuleChecker
     {
-        Puerts.DefaultLoader puerDefaultLoader;
+        Puerts.DefaultLoader puerDefaultLoader = null;
+        List<Puerts.ILoader> LoaderChain = new List<Puerts.ILoader>();
 
-        public TSLoader(string additionalBasePath): this(new string[]{ additionalBasePath }) {}
+        public TSLoader(string externalTSConfigPath): this(new string[]{ externalTSConfigPath }) {}
 
-        public TSLoader(string[] additionalBasePath = null)
+        public TSLoader(string[] externalTSConfigPath = null)
         {
-            puerDefaultLoader = new Puerts.DefaultLoader();
 #if UNITY_EDITOR
-            if (additionalBasePath != null)
+            if (externalTSConfigPath != null)
             {   
-                foreach(string path in additionalBasePath)
+                foreach(string path in externalTSConfigPath)
                 {
                     TSDirectoryCollector.AddTSCompiler(path);
                 }
             }
 #endif
+        }
+
+        public void UseRuntimeLoader(Puerts.ILoader loader)
+        {
+            LoaderChain.Add(loader);
         }
 
         public bool IsESM(string path)
@@ -31,7 +37,8 @@ namespace Puerts
 
         public virtual string Resolve(string specifier)
         {
-#if UNITY_EDITOR
+            lastResolveSpecifier = null;
+#if UNITY_EDITOR && !PUERTS_TSLOADER_DISABLE_EDITOR_FEATURE
             var fullPath = TSDirectoryCollector.TryGetFullTSPath(specifier);
             if (fullPath != null) return fullPath;
 #else
@@ -40,8 +47,24 @@ namespace Puerts
                 specifier = specifier.Replace(".mts", ".mjs").Replace(".ts", ".js");
             }
 #endif
-
-            return puerDefaultLoader.FileExists(specifier) ? specifier : null;
+            if (LoaderChain.Count == 0) 
+            {
+                if (puerDefaultLoader == null) puerDefaultLoader = new Puerts.DefaultLoader();
+                return puerDefaultLoader.FileExists(specifier) ? specifier : null;
+            }
+            else 
+            {
+                foreach (var loader in LoaderChain)
+                {
+                    if (loader.FileExists(specifier)) 
+                    {
+                        lastResolveLoader = loader;
+                        lastResolveSpecifier = specifier;
+                        return specifier;
+                    }
+                }
+                return null;
+            }
         }
 
         public bool FileExists(string filename)
@@ -49,18 +72,44 @@ namespace Puerts
             var resolveResult = Resolve(filename);
             return !string.IsNullOrEmpty(resolveResult);
         }
+        string lastResolveSpecifier;
+        Puerts.ILoader lastResolveLoader;
   
         public virtual string ReadFile(string specifier, out string debugpath)
         {
-#if UNITY_EDITOR
+#if UNITY_EDITOR && !PUERTS_TSLOADER_DISABLE_EDITOR_FEATURE
             string filepath = Resolve(specifier);
             if (filepath.EndsWith("ts")) {
                 debugpath = ""; 
                 var content = TSDirectoryCollector.EmitTSFile(filepath);
                 return content; 
             } 
+#else
+            if (specifier.EndsWith(".ts") || specifier.EndsWith(".mts"))
+            {
+                specifier = specifier.Replace(".mts", ".mjs").Replace(".ts", ".js");
+            }
 #endif
-            return puerDefaultLoader.ReadFile(specifier, out debugpath);
+            if (LoaderChain.Count == 0) 
+            {
+                return puerDefaultLoader.ReadFile(specifier, out debugpath);
+            }
+            else 
+            {
+                if (lastResolveSpecifier == specifier) 
+                {
+                    return lastResolveLoader.ReadFile(specifier, out debugpath);
+                }
+                foreach (var loader in LoaderChain)
+                {
+                    if (loader.FileExists(specifier)) 
+                    {
+                        return loader.ReadFile(specifier, out debugpath);
+                    }
+                }
+                debugpath = "";
+                return null;
+            }
         }
     }
 }
--- upm/Samples~/Sample1/Assets/Scripts/TSLoaderExample.cs
+++ upm/Samples~/Sample1/Assets/Scripts/TSLoaderExample.cs
@@ -3,12 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using Puerts;
 
+public class TestLoader: ILoader 
+{
+    public bool FileExists(string specifier) 
+    {
+        return specifier == "test.mjs";
+    }
+
+    public string ReadFile(string specifier, out string debugpath)
+    {
+        debugpath = "test.mjs";
+        return "console.log('test Runtime')";
+    }
+}
+
 public class TSLoaderExample : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        JsEnv env = new JsEnv(new TSLoader());
+        var loader = new TSLoader();
+        // UseRuntimeLoader在Runtime下会形成链式处理。在Editor下不生效。
+        // Editor下打开PUERTS_TSLOADER_DISABLE_EDITOR_FEATURE可以测试runtime下的效果。
+        loader.UseRuntimeLoader(new TestLoader());
+        // 通过菜单命令可以快速把TS构建到Resources目录供DefaultLoader使用   
+        loader.UseRuntimeLoader(new DefaultLoader());
+
+        JsEnv env = new JsEnv(loader);
+        env.ExecuteModule("test.mts");
         env.ExecuteModule("main.mts");
     }
 
